57d50038565e3e6c40dd0bd70c223989ed03e6e8
Rishi-Raj-Kalita <rishirajkalita13@gmail.com>
2020-05-22 01:24:40 +0530

    Updates

---
+++ precache-manifest.ddac13f56150d6dee690ad132101675a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce48074e3e8bcdb0282e17627a06c72",
+    "url": "/Practice2/index.html"
+  },
+  {
+    "revision": "f18de82ee8ca20120a23",
+    "url": "/Practice2/static/js/2.576b8034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Practice2/static/js/2.576b8034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cad33263f3eda45624a3",
+    "url": "/Practice2/static/js/main.14901b21.chunk.js"
+  },
+  {
+    "revision": "9a5b8dfdb372e1919c98",
+    "url": "/Practice2/static/js/runtime-main.360fdf30.js"
+  }
+]);
